9122179acee1f63a062fc279616796d69ad72890
Kono1889 <agyemangkono1889@gmail.com>
2024-12-07 16:10:17 +0000

    button

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -84,7 +84,7 @@ const Admin = () => {
                   onClick={() => handleDelete(item.id)}
                   className="ml-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors duration-300"
                 >
-                  Answered to
+                  Attended
                 </button>
             </li>
           ))
